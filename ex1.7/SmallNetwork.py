fd43770b3d66bf529c160600fa5b580d71b2005e
Iván Pulido <ijpulidos@unal.edu.co>
2016-12-27 16:15:23 -0500

    There was an error in how I was calculating the random shortcuts, now it's working according to what the book asks, I left the old random routine as a thing to try because it's fun to play with.

--- ex1.7/SmallNetwork.py
+++ ex1.7/SmallNetwork.py
@@ -27,13 +27,33 @@ class SmallNetwork(object):
         self.edges = []
         # Adding the fixed neighbors
         for i in self.nodes:
-            self.edges.append(self.get_neighbors(i))
+            self.edges.append(self.get_closest(i))
+
+        self.add_rand_shortcuts()  # This adds the random shortcuts according to what the book says
+
+    # Truly random shortcuts given probability p
+    # This is not what the book asks but it's fun to play with :)
+    def random_shortcuts(self):
+        """
+        This method adds truly random connections/edges according to the value of p. This is just for fun.
+        :return: None
+        """
         # Adding random shortcuts
         for i in self.nodes:
-            for j in range(i+1,max(self.nodes)+1):
+            for j in range(i + 1, max(self.nodes) + 1):
                 if np.random.uniform() <= self.p:
-                    self.add_edge(i,j)
+                    self.add_edge(i, j)
 
+    def add_rand_shortcuts(self):
+        """
+        Adds random shortcuts up to p*L*Z/2 as the book suggests
+        :return: None
+        """
+        num_shortcuts = round(self.p * self.L * 0.5 * self.Z) # Number of shortcuts p*L*Z/2
+        for i in range(num_shortcuts):
+            node1 = round(np.random.uniform() * (len(self.nodes)-1))
+            node2 = round(np.random.uniform() * (len(self.nodes)-1))
+            self.add_edge(node1, node2)
 
     def has_node(self, node):
         """
@@ -41,7 +61,7 @@ class SmallNetwork(object):
         :param node: Node to check if exists in the network
         :return: True if the node exists, False otherwise.
         """
-        assert type(node) is int, "Node %r is not a integer" % node  # Type should be int
+        assert type(node) is int, "Node %r is not an integer" % node  # Type should be int
         if node in self.nodes:
             return True
         else:
@@ -76,22 +96,26 @@ class SmallNetwork(object):
         """
         return self.nodes
 
-    def get_neighbors(self, node):
+    def get_closest(self, node):
         """
-        Returns the neighbors of an existing node
+        Returns the closest nodes to the given node
         :param node: Node to get neighbors from
         :return: List of neighbors of node
         """
         if self.has_node(node):
-            low_neighbor = int((node - 0.5 * self.Z) % self.L)
-            neighbors = []
+            low_closest = int((node - 0.5 * self.Z) % self.L)
+            closest = []
             for i in range(self.Z + 1):
-                neighbor = (low_neighbor + i) % self.L
+                neighbor = (low_closest + i) % self.L
                 # print(neighbor, node)
                 if neighbor == node:
                     pass
                 else:
-                    neighbors.append(neighbor)
+                    closest.append(neighbor)
         else:
             raise ValueError("Node %r is not part of the network" % node)
-        return neighbors
+        return closest
+
+    def get_neighbors(self, node):
+        assert type(node) is int, "Node represented by %r is not an integer" % node  # Type should be int
+        return self.edges[node]
